965fe69da5ad57a5a86c9fed72217e02659ed87b
AlfredoRago <alfredo.rago@gmail.com>
2019-03-06 10:45:24 +0100

    MaxGenVal now works using internal environment processing and logistic
    Also includes explicit term for proprotion of gene decay

--- MaxGenVal/maxgenval.f90
+++ MaxGenVal/maxgenval.f90
@@ -3,13 +3,14 @@ program integral ! auxiliary program, calculates the maximum gene value of an in
 implicit none
 
 integer :: i,j,k,ii,jj,kk,n,ng,t,tmax,ngs
-real*4  :: q,u,v,x,y,z,g(2),gt(2),w,env
+real*4  :: q,u,v,x,y,z,g(2),gt(2),w,env,decay
 
 tmax=20   ! developmental time
 ngs=2     ! numnber of genes
 w=1.0     ! maxval W matrix
-g=0.01    ! Initial conditions
-env=0.01  ! max environmental input
+g=1.0     ! Initial conditions
+env=0.5   ! max environmental input
+decay=0.0 ! gene degradation per iteration (proportion)
 
 open(1, file="MaxVal.tsv", status="new", action="write") ! Create output file
 write(1,*)'tMax',tmax,'nGenes',ngs,'maxMatrix',w,'initialConc',g ! Write as tab separated
@@ -18,13 +19,13 @@ close(1)
  do t=1,tmax                                               ! developmental time
         do k=1,ngs                                         ! for each gene ef this cell
           x=g(k)                                           ! concentration of gene k in cell j of ind
-          q=0.0	                                           ! REACTION
+          q=(x+env)*0.5                                    ! add and average concentration and environment
           do jj=1,ngs
               q=q+w*g(jj)
           end do
-          q=(q+env)*0.5                                    ! max environmental input=1.0
-          y=0.2*x                                          ! DEGRADATION
-          gt(k)=tanh(x-y+q)                                ! t+1      ! iterative developmental function Kostas
+          y=decay*x                                        ! DEGRADATION
+          gt(k)=tanh(x-y)                                  ! t+1      ! iterative developmental function Kostas
+          gt(k)=(gt(k)+1)*0.5                              ! rescale tanh to logistic
           if(gt(k).le.0.0)then ; gt(k)=0.0 ; end if        ! POSITIVE STATE VARIABLE. CHOOSE YOURSELF :)
       end do
 
