49bfdccba243c7a6c7081fff8b700f723c5afb2b
Arago <alfredo.rago@gmail.com>
2019-11-05 17:34:21 +0100

    corrected end of line separators for unix system

--- grns.sh
+++ grns.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
- #gfortran -w -fexceptions -fno-underscoring -fbounds-check inicio.mod.f90 diffusion.mod.f90 reaction.mod.f90 gnuplotter.mod.f90 idps.f90 -o idps.e 
-
  gfortran -w -fexceptions -fno-underscoring -Wall -Wtabs start.mod.f90 development.mod.f90 grns2.f90 -o grns.e
