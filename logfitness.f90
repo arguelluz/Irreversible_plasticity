440df40abe5d35e133b18888c4505085cfac979b
Arago <alfredo.rago@gmail.com>
2019-11-06 10:27:49 +0100

    Change line endings to UNIX format

--- logfitness.f90
+++ logfitness.f90
@@ -29,8 +29,8 @@ end do
 write(*,*)'fit2',fitness(:)
 
 fmax=maxval(fitness(:))               ! first relative fitness (0-1)
-do i=1,p 
-  fitness(i)=fitness(i)/fmax 
+do i=1,p
+  fitness(i)=fitness(i)/fmax
 end do
 write(*,*)'fit3',fitness(:)
 
@@ -41,34 +41,34 @@ do i=1,1000!p
   write(*,*)'x',x
   kk=p/2 ; k=p/2
   do j=1,logp
-    k=k/2   
+    k=k/2
     if(j.eq.logp-1)then;k=1;end if     ! allows for p non congruent log2(p)
     write(*,*)'engancha',kk,fitness(kk)
     if(x.le.fitness(kk))then
       write(*,*)'kk',k,kk,fitness(kk),'menos'
       if(j.eq.logp)then;whois=kk;end if
-      kk=kk-k      
+      kk=kk-k
       if(kk.eq.0)then ; whois=1 ; goto 754 ; end if
     else if(x.gt.fitness(kk))then
       write(*,*)'kk',k,kk,fitness(kk),'mas'
       if(j.eq.logp)then;whois=kk+1;end if
-      kk=kk+k     
+      kk=kk+k
       if(kk.gt.p)then ; whois=p ; goto 754 ; end if
-    end if         
+    end if
     write(*,*)'kk va por',kk
   end do
   754 write(*,*)'final',whois
-  
+
   kk=0
   do j=1,p
     kk=kk+1
     if(x.lt.fitness(j))then ; write(*,*)x,j,fitness(j); exit ; endif
   end do
   write(*,*)'check',kk
-  
+
   if(kk.ne.whois)then ; write(*,*)fitness(:); read(*,*) ; end if
   !fitnesst(i)=whois! fill up next generation
-  
+
   fitnesst(whois)=fitnesst(whois)+1
 end do
   fitnesst=fitnesst/maxval(fitnesst)
